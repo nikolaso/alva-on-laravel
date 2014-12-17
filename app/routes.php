74e675f1fef65ffa14f12c10696db2c7b221b88e
m0k1 <moki.forum@gmail.com>
2014-12-17 22:38:27 +0100

    Added market route

--- app/routes.php
+++ app/routes.php
@@ -16,4 +16,9 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
+Route::get('/', function()
+{
+	return View::make('market');
+});
+
 Route::get('users', 'UserController@getIndex');
